51d439a5bfec6febc5d67ec48dac08686f119c6b
tanya98 <128077502+tanyusha98@users.noreply.github.com>
2026-03-31 23:54:27 +0300

    Update weather.py

--- app/weather.py
+++ app/weather.py
@@ -1,3 +1,9 @@
 # app/weather.py
 def get_weather(city):
     return {"city": city, "temp": 20}
+def get_weather(city):
+    return {
+        "city": city,
+        "temp": 20,
+        "status": "sunny"
+    }
