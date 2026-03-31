2659a881aed99c0dccbbf9e8ac4a487ad24d88c1
tanya98 <128077502+tanyusha98@users.noreply.github.com>
2026-03-31 23:49:12 +0300

    Update tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -4,3 +4,6 @@ def add_task(task):
     tasks.append({"task": task, "done": False})
 def list_tasks():
     return tasks
+def complete_task(index):
+    if 0 <= index < len(tasks):
+        tasks[index]["done"] = True
